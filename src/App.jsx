4927e89953830c4ba8f648fd75902246f9f79fbd
Ygorokii <tsukinomusuko.58@gmail.com>
2023-10-19 21:20:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import './player.css'
+function App() {
+
+
+  return (
+    <>
+  
+  <div className='coffe'><h1>Coffee</h1></div>
+<img className='icon' src="src/café/insta pequeno.png" alt="icon" />
+
+
+  <div>
+
+<video className='video' src="src/café/coffee girls.mp4"></video>
+  </div> <player/>
+    
+    
+<div>
+  
+  </div>    
+
+
+
+
+
+</>
+  )
+}
+
+export default App
